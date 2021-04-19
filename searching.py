2baa727a1fccee9734ed638a4a0c001ebfea0a66
Lukáš Hrbotický <xhrbot00@stud.feec.vutbr.cz>
2021-04-19 11:17:13 +0200

    02

--- searching.py
+++ searching.py
@@ -17,16 +17,62 @@ def read_data(file_name, field):
     if field not in {"unordered_numbers", "ordered_numbers", "dna_sequence"}:
         return None
 
-    with open(file_path, "f") as json_file:
+    with open(file_path, "r") as json_file:
         dict = json.load(json_file)
 
     seq = dict[field]
     return seq
 
 
+def linear_search(seq, num):
+    """
+
+    :param seq:
+    :param num:
+    :return:
+    """
+    indices = []
+    count = 0
+    for position, i in enumerate(seq):
+        if i == num:
+            indices.append(position)
+            count += 1
+
+    dict = {
+        "positions": indices,
+        "count": count
+    }
+
+    return dict
+
+
+def patter_search(sequence, pattern):
+
+    pattern_size = len(pattern)
+    positions = set()
+
+    left_idx = 0
+    right_idx = pattern_size
+
+    for index, i in enumerate(sequence):
+        if pattern == sequence[index:index+pattern_size]:
+            positions.add(index + pattern_size // 2)
+
+    return positions
+
+
 def main():
+    #linear search
     file_name = "sequential.json"
-    pass
+    seq = read_data(file_name, "unordered_numbers")
+    print(seq)
+    res = linear_search(seq, num=0)
+    print(res)
+
+    #pattern search
+    sequnce = read_data(file_name, "dna_sequence")
+    res_2 = patter_search(sequnce, "ATA")
+    print(res_2)
 
 
 if __name__ == '__main__':
